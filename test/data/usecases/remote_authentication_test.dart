d7f29bc6b7dba93e1ca9844341ff8101e313990b
softwar02 <vinicius@softwar.com.br>
2021-02-01 13:55:24 -0300

    ensure RemoteAuthentication calls HttpClient with correct values

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -10,18 +10,21 @@ class RemoteAuthentication {
   RemoteAuthentication({@required this.url, @required this.httpClient});
 
   Future<void> auth() async {
-    await httpClient.request(url: url);
+    await httpClient.request(url: url, method: 'post');
   }
 }
 
 abstract class HttpClient {
-  Future<void> request({@required String url});
+  Future<void> request({
+    @required String url,
+    @required String method,
+  });
 }
 
 class HttpClientSpy extends Mock implements HttpClient {}
 
 void main() {
-  test('Should call HttpClient with correct URL', () async {
+  test('Should call HttpClient with correct values', () async {
     final httpClient = HttpClientSpy();
     final url = faker.internet.httpUrl();
 
@@ -29,6 +32,9 @@ void main() {
 
     await sut.auth();
 
-    verify(httpClient.request(url: url));
+    verify(httpClient.request(
+      url: url,
+      method: 'post',
+    ));
   });
 }
