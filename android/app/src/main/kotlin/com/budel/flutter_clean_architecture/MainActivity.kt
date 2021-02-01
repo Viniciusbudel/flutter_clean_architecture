2aea9fb4f4b56fac8d4a155abe32fc3f2ee40601
softwar02 <vinicius@softwar.com.br>
2021-02-01 12:45:36 -0300

    add Authentication UseCase

---
+++ android/app/src/main/kotlin/com/budel/flutter_clean_architecture/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.budel.flutter_clean_architecture
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/usecases/authentication.dart
@@ -0,0 +1,11 @@
+import 'package:meta/meta.dart';
+
+
+import '../domain/entities/account_entity.dart';
+
+abstract class Authentication {
+  Future<AccountEntity> auth({
+    @required String email,
+    @required String password,
+  });
+}
