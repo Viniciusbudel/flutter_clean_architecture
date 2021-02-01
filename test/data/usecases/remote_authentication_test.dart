6ff1e5ef0a62de0c27448c2594f881a648a9d376
softwar02 <vinicius@softwar.com.br>
2021-02-01 16:12:30 -0300

    ensure RemoteAuthentication returns an account on 200

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -25,6 +25,16 @@ void main() {
   });
 
   test('Should call HttpClient with correct values', () async {
+    when(httpClient.request(
+        url: anyNamed('url'),
+        method: anyNamed('method'),
+        body: anyNamed('body')))
+        .thenAnswer((_) async => {
+      'accessToken': faker.guid.guid(),
+      'name': faker.person.name(),
+    });
+
+
     await sut.auth(params);
 
     verify(httpClient.request(
@@ -68,7 +78,9 @@ void main() {
 
     expect(future, throwsA(DomainError.unexpected));
   });
-  test('Should throw InvalidCredentialsError if HttpClient return 401', () async {
+
+  test('Should throw InvalidCredentialsError if HttpClient return 401',
+      () async {
     when(httpClient.request(
             url: anyNamed('url'),
             method: anyNamed('method'),
@@ -78,5 +90,21 @@ void main() {
     final future = sut.auth(params);
 
     expect(future, throwsA(DomainError.invalidCredentials));
+  });
+  test('Should return a Account if HttpClient return 200', () async {
+    final accessToken = faker.guid.guid();
+
+    when(httpClient.request(
+            url: anyNamed('url'),
+            method: anyNamed('method'),
+            body: anyNamed('body')))
+        .thenAnswer((_) async => {
+              'accessToken': accessToken,
+              'name': faker.person.name(),
+            });
+
+    final account = await sut.auth(params);
+
+    expect(account.token, accessToken);
   });
 }
