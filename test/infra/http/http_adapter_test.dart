ef8df035903b12b034c0948cf34042ee37b1045f
softwar02 <vinicius@softwar.com.br>
2021-02-01 18:00:42 -0300

    ensure HttpAdapter calls post without body

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -22,7 +22,8 @@ class HttpAdapter {
       'content-type': 'application/json',
       'accept': 'application/json',
     };
-    await client.post(url, headers: headers,body: jsonEncode(body));
+    final jsonBody = body != null ? jsonEncode(body) : null;
+    await client.post(url, headers: headers, body: jsonBody);
   }
 }
 
@@ -39,16 +40,25 @@ void main() {
 
   group('post', () {
     test('Should call post with correct values', () async {
-      await sut.request(url: url, method: 'post',body: {'any_key' : 'any_value'});
+      await sut.request(
+          url: url, method: 'post', body: {'any_key': 'any_value'});
 
       verify(client.post(
-        url,
-        headers: {
-          'content-type': 'application/json',
-          'accept': 'application/json',
-        },
-        body: '{"any_key":"any_value"}'
+          url,
+          headers: {
+            'content-type': 'application/json',
+            'accept': 'application/json',
+          },
+          body: '{"any_key":"any_value"}'
       ));
     });
   });
+
+  test('Should call post without body', () async {
+    await sut.request(url: url, method: 'post');
+
+    verify(client.post(
+        url,
+        headers: anyNamed('headers')));
+    });
 }
