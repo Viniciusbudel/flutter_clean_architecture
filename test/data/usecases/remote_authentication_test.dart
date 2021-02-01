84f789087115767eecc1d5e010a6c537aa1ccaab
softwar02 <vinicius@softwar.com.br>
2021-02-01 15:11:42 -0300

    ensure RemoteAuthentication throws unexpected error when 500

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -44,6 +44,7 @@ void main() {
 
     expect(future, throwsA(DomainError.unexpected));
   });
+
   test('Should throw UnexpectedError if HttpClient return 404', () async {
     when(httpClient.request(
             url: anyNamed('url'),
@@ -54,5 +55,28 @@ void main() {
     final future = sut.auth(params);
 
     expect(future, throwsA(DomainError.unexpected));
+  });
+
+  test('Should throw UnexpectedError if HttpClient return 500', () async {
+    when(httpClient.request(
+            url: anyNamed('url'),
+            method: anyNamed('method'),
+            body: anyNamed('body')))
+        .thenThrow(HttpError.serverError);
+
+    final future = sut.auth(params);
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
+  test('Should throw InvalidCredentialsError if HttpClient return 401', () async {
+    when(httpClient.request(
+            url: anyNamed('url'),
+            method: anyNamed('method'),
+            body: anyNamed('body')))
+        .thenThrow(HttpError.unauthorized);
+
+    final future = sut.auth(params);
+
+    expect(future, throwsA(DomainError.invalidCredentials));
   });
 }
