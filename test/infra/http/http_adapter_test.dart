33c21c7b90b0e99c26649c611dc2462d387d54dd
softwar02 <vinicius@softwar.com.br>
2021-02-02 17:54:23 -0300

    ensure http adapter returns ServerError if post return 500

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -82,13 +82,21 @@ void main() {
 
       expect(response, null);
     });
-    
+
     test('Should return BadRequestError if post returns 400', () async {
       mockResponse(400);
 
       final future = sut.request(url: url, method: 'post');
 
       expect(future, throwsA(HttpError.badRequest));
+    });
+
+    test('Should return BadRequestError if post returns 400', () async {
+      mockResponse(500);
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.serverError));
     });
   });
 }
