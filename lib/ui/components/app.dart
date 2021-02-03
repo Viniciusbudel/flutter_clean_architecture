dbb82c19d40e459b72df8b8f17bdced2f7d4e830
viniciusbudel <vini_budel@hotmail.com>
2021-02-02 22:54:01 -0300

    add LoginPage

---
+++ lib/ui/components/app.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import '../pages/pages.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final primaryColor = Color.fromRGBO(136, 14, 79, 1);
+    final primaryColorDark = Color.fromRGBO(96, 0, 39, 1);
+    final primaryColorLigth = Color.fromRGBO(188, 71, 123, 1);
+
+    return MaterialApp(
+      title: '4Dev',
+      theme: ThemeData(
+          primaryColor: primaryColorDark,
+          primaryColorDark: primaryColorDark,
+          primaryColorLight: primaryColorLigth,
+          accentColor: primaryColor,
+          backgroundColor: Colors.white,
+          textTheme: TextTheme(
+            headline1: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: primaryColorDark),
+          ),
+          inputDecorationTheme: InputDecorationTheme(
+              enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: primaryColorLigth),
+              ),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: primaryColor),
+              ),
+              alignLabelWithHint: true),
+          buttonTheme: ButtonThemeData(
+              colorScheme: ColorScheme.light(primary: primaryColor),
+              buttonColor: primaryColor,
+              splashColor: primaryColorLigth,
+              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              textTheme: ButtonTextTheme.primary,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20)))),
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
+    );
+  }
+}
