8937bdb56bff051d2ae83a8c23a70b83cb37dd35
Viniciusbudel <vini_budel@hotmail.com>
2021-02-05 13:38:18 -0300

    move Validation and StreamLoginPresenter to his own files

--- test/presentation/presenters/stream_login_presenter_test.dart
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -1,48 +1,21 @@
-import 'dart:async';
-
 import 'package:faker/faker.dart';
-import 'package:meta/meta.dart';
+import 'package:flutter_clean_architecture/presentation/presenters/presenters.dart';
+import 'package:flutter_clean_architecture/presentation/protocols/protocols.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
-abstract class Validation {
-  String validate({@required String field, @required String value});
-}
-
-class LoginState {
-  String emailError;
-}
-
-class StreamLoginPresenter {
-  final Validation validation;
-  final _controller = StreamController<LoginState>.broadcast();
-
-  var _state = LoginState();
-
-  Stream<String> get emailErrorStream =>
-      _controller.stream.map((state) => state.emailError);
-
-  StreamLoginPresenter({@required this.validation});
-
-  void validateEmail(String email) {
-    _state.emailError = validation.validate(field: 'email', value: email);
-    _controller.add(_state);
-  }
-}
-
 class ValidationSpy extends Mock implements Validation {}
-
 
 main() {
   StreamLoginPresenter sut;
   ValidationSpy validation;
   String email;
 
-  PostExpectation mockValidationCall(String field) => when(
-      validation.validate(field: field == null ? anyNamed('field') : field, value: anyNamed('value')));
+  PostExpectation mockValidationCall(String field) => when(validation.validate(
+      field: field == null ? anyNamed('field') : field,
+      value: anyNamed('value')));
 
-
-  void mockValidation({String field,String value}){
+  void mockValidation({String field, String value}) {
     mockValidationCall(field).thenReturn(value);
   }
 
@@ -52,7 +25,6 @@ main() {
     email = faker.internet.email();
 
     mockValidation();
-
   });
 
   test('Should call Validation with correct email', () {
