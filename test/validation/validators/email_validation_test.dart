459365482211faab84a076f38685bf26abccde57
Viniciusbudel <vini_budel@hotmail.com>
2021-02-05 17:37:13 -0300

    ensure EmailValidation returns error if email is invalid

--- test/validation/validators/email_validation_test.dart
+++ test/validation/validators/email_validation_test.dart
@@ -7,7 +7,10 @@ class EmailValidation implements FieldValidation {
   EmailValidation(this.field);
 
   String validate(String value) {
-    return null;
+    final regex = RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+    final isValid = value?.isNotEmpty != true || regex.hasMatch(value) ;
+
+    return isValid ? null : 'Campo inválido';
   }
 }
 
@@ -27,5 +30,9 @@ void main() {
 
   test('Should return null if email is valid', () {
     expect(sut.validate('viniciuhbudel@gmail.com'), null);
+  });
+
+  test('Should return error if email is invalid', () {
+    expect(sut.validate('viniciuhbudel'), 'Campo inválido');
   });
 }
