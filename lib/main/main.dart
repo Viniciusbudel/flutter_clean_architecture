e37e59489beb5669497a35dd05d1102684751d7a
Viniciusbudel <vini_budel@hotmail.com>
2021-02-09 15:26:48 -0300

    ensure LoginValidationsFactory returns correct validations

--- lib/main/main.dart
+++ lib/main/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
-import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 import '../ui/components/components.dart';
 import 'factories/factories.dart';
---
+++ test/main/factories/pages/login/login_validation_factory_test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_clean_architecture/main/factories/factories.dart';
+import 'package:flutter_clean_architecture/validation/validators/validators.dart';
+import 'package:test/test.dart';
+
+main() {
+  test('Should return the correct validations', () {
+    final validations = makeLoginValidations();
+
+    expect(validations, [
+      RequiredFieldValidation('email'),
+      RequiredFieldValidation('password'),
+      EmailValidation('email'),
+    ]);
+  });
+}
