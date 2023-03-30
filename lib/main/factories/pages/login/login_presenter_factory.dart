12de175c0b8536ce7e4277f281b3dbffdf69983a
Vinicius Budel <vinicius.budel@positivo.com.br>
2023-03-30 19:25:21 -0300

    remove StreamLoginPresenter

--- lib/main/factories/pages/login/login_presenter_factory.dart
+++ lib/main/factories/pages/login/login_presenter_factory.dart
@@ -1,17 +1,12 @@
+import '../../../../presentation/presenters/getx_login_presenter.dart';
 import '../../../../presentation/presenters/presenters.dart';
 
 import '../../../../ui/pages/pages.dart';
 
 import '../../factories.dart';
 
-LoginPresenter makeLoginPresenter() {
-  return StreamLoginPresenter(
-      validation: makeLoginValidation(),
-      authentication: makeRemoteAuthentication());
-}
-
 LoginPresenter makeGetxLoginPresenter() {
-  return StreamLoginPresenter(
+  return GetxLoginPresenter(
       validation: makeLoginValidation(),
       authentication: makeRemoteAuthentication());
 }
